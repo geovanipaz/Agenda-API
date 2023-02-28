32365d31a4d91dfa0f02aa2310056d57760c078a
geovani <geovanipaz7@gmail.com>
2023-02-28 19:33:49 -0300

    edição de agendamentos

--- agenda/views.py
+++ agenda/views.py
@@ -2,20 +2,46 @@ from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
 from agenda.models import Agendamento
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from agenda.serializers import AgendamentoSerializer
 # Create your views here.
 
-@api_view(http_method_names=["GET"])
+@api_view(http_method_names=["GET","PATCH","DELETE"])
 def agendamento_detail(request, id):
     obj = get_object_or_404(Agendamento, id=id)
-    serializer = AgendamentoSerializer(obj)
-    return JsonResponse(serializer.data)
+    if request.method == "GET":
+        
+        serializer = AgendamentoSerializer(obj)
+        return JsonResponse(serializer.data)
+    if request.method == "PATCH":
+        
+        serializer = AgendamentoSerializer(data=request.data, partial=True)
+        if serializer.is_valid():
+            
+            serializer.save()
+            return JsonResponse(serializer.data, status=200)
+        return JsonResponse(serializer.errors, status=400)
+    if request.method == "DELETE":
+        
+        obj.delete()
+        return Response(status=204)
 
-@api_view(http_method_names=["GET"])
+
+@api_view(http_method_names=["GET","POST"])
 def agendamento_list(request):
-    qs = Agendamento.objects.all()
-    serializer = AgendamentoSerializer(qs, many=True)
-    return JsonResponse(serializer.data, safe=False)
+    if request.method == "GET":
+        qs = Agendamento.objects.all()
+        serializer = AgendamentoSerializer(qs, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    if request.method == "POST":
+        data = request.data
+        serializer = AgendamentoSerializer(data=data)
+        if serializer.is_valid():
+            validated_data = serializer.validated_data
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=404)
+
 
 
